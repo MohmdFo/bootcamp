f771c886e178068e54bc67aee9dd6a87c2489447
Babak <Babakzare2250@gmail.com>
2023-01-28 18:56:55 +0330

    style(bootcamp): fix flake8 hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 print("hello world")
 fullname = "mohammad fotouhi"
 age = 21
-<<<<<<< HEAD
-dragon="i will eat you"
-=======
+dragon = "i will eat you"
 message = "Hello Django guys!"
->>>>>>> 473a54c18745cc4fb3f55d18ff6b365d4efe4879
